fa9cbbe258637e0e2ab47a09650b2ee73d5147a9
lumertzjoao <joaolumertz15@gmail.com>
2024-12-02 18:54:35 -0300

    Terminado

--- apps/main.c
+++ apps/main.c
@@ -18,6 +18,22 @@ int option = 0;
 
 int main(void) {
     List *L = listCreate();
+
+    // Criando os contatos;
+    Contacts *c1 = contactCreate("Alice", "alice@example.com", "123456789", "999.999.999-99");
+    Contacts *c2 = contactCreate("Bob", "bob@example.com", "987654321", "555.555.555-55");
+    Contacts *c3 = contactCreate("Charlie", "charlie@example.com", "456123789", "666.666.666-66");
+    Contacts *c4 = contactCreate("Dave", "dave@example.com", "741852963", "333.333.333-33");
+    Contacts *c5 = contactCreate("Eve", "eve@example.com", "852963741", "888.888.888-88");
+    Contacts *c6 = contactCreate("Carlos", "carlos@gmail.com", "326547898", "222333666625");
+
+    listInsert(L, c1); // Final, lista vaiza inicialmente.
+    listInsert(L, c2); // Final
+    listInsert(L, c3); // Meio
+    listInsert(L, c4); // Começo
+    listInsert(L, c5); // Entre começo e meio
+    listInsert(L, c6);
+
     while (option != 5) {
         run(L);
         option = 0;
